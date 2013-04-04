5494d28ddaacd766cf69334dd2431f53fa5db69f
Nathan W Brei <nathan.w.brei@gmail.com>
2013-04-04 11:43:01 -0400

    Basic pieces of auth+fbauth

--- trussvis.py
+++ trussvis.py
@@ -1,12 +1,73 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, url_for, redirect, escape
+from flask_oauth import OAuth
+
 import json
 import sys
 import argparse
 import brain
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+# Create OAuth object
+oauth = OAuth()
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key='109598425902918',
+    consumer_secret='21599ff47fc62fcbee95a5b3453f5a63',
+    request_token_params={'scope': 'email'}
+)
+
+
+@app.route('/user')
+def stupid():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+@app.route('/login2', methods=['GET', 'POST'])
+def login2():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect('/user')
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
 
 @app.route('/trussvis', methods=['POST'])
 def trussvis():
@@ -26,4 +87,4 @@ def mainpage():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
