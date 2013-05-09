0aba7179487c294da073246fbc4224f75f4e9ba7
Nathan Brei <nathan.w.brei@gmail.com>
2013-05-09 11:12:28 -0400

    Added axes.

--- static/vis.js
+++ static/vis.js
@@ -14,7 +14,8 @@ $(document).ready(function(){
     height = 800;
     svg = d3.select("svg")
         .attr("viewBox", "0 0 " + width + " " + height )
-        .attr("preserveAspectRatio", "xMinYMin meet");
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("class", "axis")
 
     // Set scales, somehow. This needs to be redone.
     // xmin = d3.min(interfaces, function(d){return d['x'];});
@@ -29,9 +30,19 @@ $(document).ready(function(){
     yscale = d3.scale.linear()
                 .domain([ymin, 2/3*xmax+ymin])
                 .range([height, 0]);
+    xaxis = d3.svg.axis().scale(xscale).ticks(10).orient("down");
+    yaxis = d3.svg.axis().scale(yscale).ticks(8).orient("right");
 
-    redraw()
+    svg.append("svg:g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0, 500)")
+              .call(xaxis);
+    svg.append("svg:g")
+              .attr("class", "y axis")
+              .attr("transform", "translate(400,0)")
+              .call(yaxis);
         
+    redraw()
     controller2.typer.setfocus()
     });
 
